1be2ca4717178d3dacbc072e7a08796c6c81d5e3
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2026-01-30 22:37:08 -0300

    feat: add database schema for habits and week days management

---
+++ schemas.sql
@@ -0,0 +1,30 @@
+create table public.habits (
+  id uuid not null default gen_random_uuid (),
+  title text not null,
+  created_at timestamp with time zone null default now(),
+  constraint habits_pkey primary key (id)
+) TABLESPACE pg_default;
+
+create table public.week_days (
+  id smallint not null,
+  title text not null,
+  constraint week_days_pkey primary key (id)
+) TABLESPACE pg_default;
+
+INSERT INTO "public"."week_days" ("id", "title") 
+VALUES 
+    ('0', 'Domingo'), 
+    ('1', 'Segunda-feira'), 
+    ('2', 'Terça-feira'), 
+    ('3', 'Quarta-feira'), 
+    ('4', 'Quinta-feira'), 
+    ('5', 'Sexta-feira'), 
+    ('6', 'Sábado');
+
+create table public.habit_week_days (
+  habit_id uuid not null,
+  week_day_id smallint not null,
+  constraint habit_week_days_pkey primary key (habit_id, week_day_id),
+  constraint fk_habit foreign KEY (habit_id) references habits (id) on delete CASCADE,
+  constraint fk_week_day foreign KEY (week_day_id) references week_days (id)
+) TABLESPACE pg_default;
